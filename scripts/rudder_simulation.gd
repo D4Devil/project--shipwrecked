7dfaf0f8b316d4848009d3557b9deab58b335396
D4Devil <dmdio1177@gmail.com>
2024-08-25 18:16:27 -0600

    Added MVP to RudderSimulation

--- scripts/rudder_simulation.gd
+++ scripts/rudder_simulation.gd
@@ -1,1 +1,18 @@
 extends Node
+
+@export var max_spin_angle: float = 180
+@export var spin_speed: float = 100
+@export var max_rotation_angle: float = 45
+var _current_spin: float = 0
+var _current_direction_angle: float = 0 
+
+
+signal rudder_spun(current_spin: float, current_direction_angle: float)
+
+## direction_factor with it's sing the direction of the rotation, while 
+## the value is used as a speed factor
+func spin(direction_factor: float) -> void:
+	var target_spin = _current_spin + direction_factor * spin_speed
+	_current_spin = clampf(target_spin, -max_spin_angle, max_spin_angle)
+	_current_direction_angle = max_rotation_angle / (max_spin_angle) * _current_spin
+	rudder_spun.emit(_current_spin, _current_direction_angle)
