92105e583e0af9c0a32178fab4a96db7db15af27
D4Devil <dmdio1177@gmail.com>
2024-09-03 21:59:08 -0600

    Doc & Spline mvp (#5)
    * Added MVP mesh triangles generation
    
    * Sail Simulation documented
    
    * Rudder Sim documented

--- scripts/sail_simulation.gd
+++ scripts/sail_simulation.gd
@@ -1,19 +1,33 @@
 class_name SailSimulation
 extends Node
+## Node that controls a set of speeds, like gears in a car's transmision. [br]
+## Simulates the different states of a deployed sail (Not deployed, half-deployed, fully-deployed, etc.)
+##
+## Uses the _physics_process callback to constantly push a PhysicsBody3D towards it's own
+## forward direction.
 
-@export var _sail_speeds : Array[float]
-@export var _current_sail_idx : int = 0
+
+## [Dependency]: To whom this simulation will be applied
 @export var body: PhysicsBody3D
 
+## Your custom set of speeds
+@export var _sail_speeds : Array[float] = [0, 30, 70,]
+
+## The cached position of the sail state 
+@export var _current_sail_idx : int = 0
+
+
+## Emmited ONLY when a sail shift occurs succesfully.
+## Contains the state (idx), and the new sailing speed
 signal sail_changed(idx: int, speed: float)
 
 
-## Shift up (positive) or down (negative) x positions on the sail speeds array
+## Shifts up (positive) or down (negative) x positions on the sail speeds array
 ## ignore if the opertation is out of range
 func sail_shift(shitfts := 1) -> void:
 	var target_idx = _current_sail_idx + shitfts
 
-	if target_idx < 0 or target_idx >= _sail_speeds.size():
+	if target_idx < 0 or target_idx >= _sail_speeds.size(): ## Invalid sail shift
 		push_warning("Sail shift cannot be performed, index out of range.")
 		return 
 	
@@ -21,6 +35,10 @@ func sail_shift(shitfts := 1) -> void:
 	sail_changed.emit(_current_sail_idx, _sail_speeds[_current_sail_idx])
 
 
-func _physics_process(delta):
+func _physics_process(delta: float):
 	body = body as CharacterBody3D
 	body.move_and_collide(-body.basis.z * _sail_speeds[_current_sail_idx] * delta)
+
+
+func get_current_speed() -> float:
+	return _sail_speeds[_current_sail_idx]
