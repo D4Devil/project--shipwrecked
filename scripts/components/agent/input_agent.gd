01041b8b36ab7eb61ba92c41561521291cd347f9
D4Devil <dmdio1177@gmail.com>
2024-09-06 18:01:01 -0600

    Weapon set active input handled

--- scripts/components/agent/input_agent.gd
+++ scripts/components/agent/input_agent.gd
@@ -3,12 +3,13 @@ extends Node
 @export var rudder : RudderSimulation
 @export var rudder_speed: float
 @export var sail : SailSimulation
+@export var weapon: Weapon
 
 
 func _physics_process(_delta: float) -> void:
 	sail_input_handle()
 	rudder_input_handle()
-
+	weapon_input_handle()
 
 func sail_input_handle() -> void:
 	if Input.is_action_pressed("in_game_r_b"):
@@ -21,3 +22,11 @@ func sail_input_handle() -> void:
 func rudder_input_handle() -> void:
 	var axis = Input.get_axis("in_game_l_left", "in_game_l_right")
 	rudder.spin(axis * rudder_speed)
+
+
+func weapon_input_handle():
+	if Input.is_action_just_pressed("in_game_l_t"):
+		weapon._active = true
+	
+	if Input.is_action_just_released("in_game_l_t"):
+		weapon._active = false
