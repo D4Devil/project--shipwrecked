aa7741d6b0d7c60efb3a13d8ab307d5ef6fd40a6
D4Devil <dmdio1177@gmail.com>
2024-09-06 21:10:16 -0600

    Input handlers (#11)
    * Ship navigation input handlers
    
    * Weapon set active input handled

--- scripts/components/agent/input_agent.gd
+++ scripts/components/agent/input_agent.gd
@@ -3,17 +3,30 @@ extends Node
 @export var rudder : RudderSimulation
 @export var rudder_speed: float
 @export var sail : SailSimulation
+@export var weapon: Weapon
 
 
-func _unhandled_input(event):
-	if event.is_action("ui_left"):
-		rudder.spin(-1 * rudder_speed)
-	
-	if event.is_action("ui_right"):
-		rudder.spin(rudder_speed)
+func _physics_process(_delta: float) -> void:
+	sail_input_handle()
+	rudder_input_handle()
+	weapon_input_handle()
 
-	if event.is_action_pressed("ui_accept"):
+func sail_input_handle() -> void:
+	if Input.is_action_pressed("in_game_r_b"):
 		sail.sail_shift(1)
 
-	if event.is_action_pressed("ui_cancel"):
+	if Input.is_action_pressed("in_game_l_b"):
 		sail.sail_shift(-1)
+
+ 
+func rudder_input_handle() -> void:
+	var axis = Input.get_axis("in_game_l_left", "in_game_l_right")
+	rudder.spin(axis * rudder_speed)
+
+
+func weapon_input_handle():
+	if Input.is_action_just_pressed("in_game_l_t"):
+		weapon._active = true
+	
+	if Input.is_action_just_released("in_game_l_t"):
+		weapon._active = false
